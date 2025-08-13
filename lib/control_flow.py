821f4f1b28ca919c2f04168d15a3bcccc9c70e15
lewys-miugo <lewiswambugu01@gmail.com>
2025-08-13 15:34:40 +0300

    Intepreting js to python

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -1,17 +1,52 @@
-#!/usr/bin/env python3
+# !/usr/bin/env python3
 
 def admin_login(username, password):
     # your code here
-    pass
+    if (username == "admin" or username == "ADMIN") and password == "12345":
+        return "Access granted"
+    else:
+        return "Access denied"
+
+    # pass
 
 def hows_the_weather(temperature):
     # your code here
-    pass
+    if temperature<40:
+        response="brisk"
+    elif temperature>=40 and temperature <=65:
+        response="a little chilly"
+    elif temperature > 85:
+        response = "too dang hot"
+    else:
+        response = "perfect"
+
+    return f"It's {response} out there!"
+    # pass
 
 def fizzbuzz(num):
     # your code here
-    pass
+    if num % 3 == 0 and num % 5 == 0:
+        return "FizzBuzz"
+    elif num % 3 == 0:
+        return "Fizz"
+    elif num % 5 == 0:
+        return "Buzz"
+    else:
+        return num
+    # pass
 
 def calculator(operation, num1, num2):
     # your code here
-    pass
+    if operation == "+":
+        return num1 + num2
+    elif operation == "-":
+        return num1-num2
+    elif operation == "*":
+        return num1*num2
+    elif operation == "/":
+        return num1/num2
+    else:
+        print("Invalid operation!")
+        return None
+    
+    # pass
